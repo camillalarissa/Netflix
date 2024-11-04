799458c0cf75c648cebf1c3a0bf1ce5584ba581d
camillalarissa <115382914+camillalarissa@users.noreply.github.com>
2024-11-04 15:33:56 -0300

    netfix

---
+++ scripts.js
@@ -0,0 +1,35 @@
+let botaoSom = document.querySelector(".botao-som")
+let video = document.querySelector(".video")
+let botao = document.querySelector(".link-info")
+let modal = document.querySelector(".modal")
+let audio = document.querySelector(".audio")
+
+// Ligar o SOM
+botaoSom.addEventListener("click", ligaSom)
+
+function ligaSom(){
+    video.muted =  !video.muted
+}
+
+
+// Mostrar o MODAL
+
+botao.addEventListener("click", mostrarModal)
+modal.addEventListener("click", esconderModal)
+
+function mostrarModal(){
+    modal.style.display = "block"
+}
+
+function esconderModal(){
+    modal.style.display = "none"
+}
+
+// Tocar o TUNDUM
+
+window.addEventListener("load", tocarAudio)
+
+
+function tocarAudio(){
+    audio.play()
+}
